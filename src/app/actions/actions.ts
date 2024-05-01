8d637ed66f1018d24f3b1b52eb5ac88fd5a4d57e
Asim Ahmad <asim.ahmad@wildfork.com>
2024-05-01 02:05:06 -0400

    vercel fix

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -5,9 +5,9 @@ import {z} from "zod";
 import fs from 'fs';
 import path from 'path';
 
-const filePathWords = path.resolve('/', 'agwordlist.txt');
+const filePathWords = path.resolve(process.cwd()+'/src/app/assets', 'agwordlist.txt');
 const wordsList = fs.readFileSync(filePathWords, 'utf-8');
-const filePathSyllables = path.resolve('/', 'agsyllables.txt');
+const filePathSyllables = path.resolve(process.cwd()+'/src/app/assets', 'agsyllables.txt');
 const syllablesList = fs.readFileSync(filePathSyllables, 'utf-8');
 export async function createPassword(
   prevState: {
